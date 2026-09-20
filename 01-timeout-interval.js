8d9c3960286dda0cbd9fccb35ac8a910cc437fdb
selinjax1992 <selinjax1992@gmail.com>
2026-09-20 21:36:39 +0000

    Deploying to gh-pages from @ selinjax1992/practice-lesson-10-setTimeout-setInterval-date@f7f312681b38fefa5e9c615e4bf58189d1231509 🚀

--- 01-timeout-interval.js
+++ 01-timeout-interval.js
@@ -1,2 +1,2 @@
-import"./assets/reset-D65bUqBF.js";console.log("Start");const o=setInterval(()=>{console.log("Hello from setInterval")},1e3);addEventListener("keydown",e=>{e.code==="Escape"&&clearInterval(o)});console.log("End");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                     */console.log("Start");const o=setInterval(()=>{console.log("Hello from setInterval")},1e3);addEventListener("keydown",e=>{e.code==="Escape"&&clearInterval(o)});console.log("End");
 //# sourceMappingURL=01-timeout-interval.js.map
